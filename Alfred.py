d8e0073469afca70db1725a82dcabae14eaea8c7
A Puma on a Penguin <kikefontanlorenzo@gmail.com>
2018-09-25 13:43:32 +0200

    :mans_shoe: More elegant Alfred

--- Alfred.py
+++ Alfred.py
@@ -1,21 +1,25 @@
 import sys
-from NewLexer import Lexer
+
+# Import the core of the program
+from Lexer import Lexer
 from Parser import Parser
 from Evaluator import *
 
+# Debug flag to show logs
 debug = False
 
 class Alfred:
     def __init__(self):
-
+        # Implement some commands
         self.keywords = {
             'ADIOS': halt,
             'DI': echo,
             'VETE': goto
         }
-
+        # Create the lexer and the parser
         self.lexer = Lexer(self.keywords.keys())
         self.parser = Parser()
+        # Read all the files
         for file_ in sys.argv[1:]:
             self.readLine(file_)
 
@@ -27,16 +31,16 @@ class Alfred:
                 # Tokenize the line of code with the lexer
                 self.lexer.tokenizer(lineofcode)
                 if debug: print('LEXER', self.lexer.tokens)
+                # Build the program with the parser
                 self.parser.build(self.lexer.tokens)
-        
+        # Some debug logs
         if debug:
             print('INSTRUCTION POINTER', self.parser.ipointer)
             print('JUMP TABLE', self.parser.jumps)
             print('INSTRUCTION STACK', self.parser.instructions)
-        
+        # Run the built program with the evaluator
         self.evaluator = Evaluator(self.parser.jumps, self.parser.instructions, self.keywords)
-        self.evaluator.run()            
-
+        self.evaluator.run()
 
 
 if __name__ == '__main__':
