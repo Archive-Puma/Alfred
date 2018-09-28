5bd314d0bb28d827d9098ab17e451562defea748
Sherlock Puma <kikefontanlorenzo@gmail.com>
2018-09-28 10:54:58 +0200

    :tv: More instructions! (Learn, Show)

--- config/support/en.py
+++ config/support/en.py
@@ -5,11 +5,13 @@ Author: @CosasDePuma <kikefontanlorenzo@gmail.com>(https://github.com/cosasdepum
 import lib.std as lib
 
 KEYWORDS = {
-    'BYE': lib.halt,
-    'SAY': lib.echo,
-    'GO': lib.goto
+    'BYE':      lib.halt,
+    'GO':       lib.goto,
+    'LEARN':    lib.learn,
+    'SAY':      lib.echo,
+    'SHOW':     lib.show
 }
 
 CONTROLFLOW_KEYWORDS = {
-    'label': 'REMEMBER THIS MOMENT AS'
+    'label':    'REMEMBER THIS MOMENT AS'
 }
--- config/support/es.py
+++ config/support/es.py
@@ -5,11 +5,13 @@ Author: @CosasDePuma <kikefontanlorenzo@gmail.com>(https://github.com/cosasdepum
 import lib.std as lib
 
 KEYWORDS = {
-    'ADIOS': lib.halt,
-    'DI': lib.echo,
-    'VETE': lib.goto
+    'ADIOS':    lib.halt,
+    'APRENDE':  lib.learn,
+    'DI':       lib.echo,
+    'MUESTRA':  lib.show,
+    'VETE':     lib.goto
 }
 
 CONTROLFLOW_KEYWORDS = {
-    'label': 'RECUERDA ESTE MOMENTO COMO'
+    'label':    'RECUERDA ESTE MOMENTO COMO'
 }
