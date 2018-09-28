b80920c30900f3aa79f5bfa7629c7006bddb825d
Sherlock Puma <kikefontanlorenzo@gmail.com>
2018-09-28 10:47:22 +0200

    :ledger: Learn and show new functions (Modular libs)

--- lib/std.py
+++ lib/std.py
@@ -4,6 +4,9 @@ Author: @CosasDePuma <kikefontanlorenzo@gmail.com>(https://github.com/cosasdepum
 
 # pylint: disable=W0613
 
+import re
+from importlib import import_module
+
 def echo(args, evaluator):
     """ Show a message through the stdin """
     print(' '.join(args))
@@ -17,3 +20,35 @@ def goto(args, evaluator):
 def halt(args, evaluator):
     """ Stop the program with a clean exit """
     exit(0)
+
+def learn(args, evaluator):
+    """ Stop the program with a clean exit """
+    lib = ' '.join(args[1:]).upper()
+    lang = evaluator.lang.lower() + '-' if evaluator.lang else ''
+    evaluator.keywords[lib] = import_module('lib.' + lang + lib.lower()).Lib()
+
+def show(commands, evaluator):
+    """ Show the output returned after running a module """
+    options = {}
+    instruction = None
+    # Find the instruction
+    for cmd in commands:
+        if not instruction:
+            cmd = cmd.upper()
+            if cmd in evaluator.keywords.keys():
+                instruction = evaluator.keywords[cmd]
+    # Get the arguments
+    if instruction:
+        txt = ' '.join(commands).upper()
+        for arg in instruction.args:
+            # Find the next word after the argument
+            matches = re.search('({0})\\ \\w+'.format(arg), txt).group()
+            matches = re.sub('({0})\\ '.format(arg), '', matches)
+            # Create the args dictionary
+            if matches:
+                options = {
+                    arg.upper(): matches
+                }
+    # Run and show the module
+    instruction.run(options)
+    instruction.show()
