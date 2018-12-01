dc962d7a67d826f9c3f3b8c46c708c59a7618b24
Sherlock Puma <kikefontanlorenzo@gmail.com>
2018-12-01 14:04:33 +0100

    :globe_with_meridians: Spanish Language

---
+++ src/Lang/ES.hs
@@ -0,0 +1,16 @@
+module Lang.ES where
+
+lang_space  = " "
+
+lang_as     = "como"
+lang_aldred = "alfred"
+lang_define = "define"
+lang_go     = "vete"
+lang_jump   = "salta"
+lang_print  = "escribe"
+lang_show   = "muestra"
+lang_to     = "a"
+lang_value  = "el valor de"
+
+lang_bye    = "adios" ++ lang_space ++ lang_aldred
+lang_moment = lang_define ++ lang_space ++ "el momento"
