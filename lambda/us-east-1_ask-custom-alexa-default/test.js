2c1e3629e8c1a1196814c1ea8bc60bfc25389f6a
MiniPuma <kikefontanlorenzo@gmail.com>
2018-12-14 00:46:23 +0100

    :white_check_mark: More Tests!
    - Help
    - Stop
    - Command: Di
    - Command: Adios Alfred

--- lambda/us-east-1_ask-custom-alexa-default/test.js
+++ lambda/us-east-1_ask-custom-alexa-default/test.js
@@ -8,12 +8,14 @@ const DIALOGS = {
   'speak':  [ '<speak>', '</speak>' ],
   'prompt': '¿Qué instrucción deseas que ejecute?',
 
-  'LaunchRequest': "Bienvenido al intérprete por voz del lenguaje de programación Alfred. Ejecutaré las instrucciones que me vayas diciendo de una en una. Si necesitas ayuda prueba a decir \"Ayuda\". Si deseas salir de la Skill di \"Salir\" o ejecuta el comando \"Adiós Alfred\". ¿Qué deseas hacer?"
+  'LaunchRequest':  "Bienvenido al intérprete por voz del lenguaje de programación Alfred. Ejecutaré las instrucciones que me vayas diciendo de una en una. Si necesitas ayuda prueba a decir \"Ayuda\". Si deseas salir de la Skill di \"Salir\" o ejecuta el comando \"Adiós Alfred\". ¿Qué deseas hacer?",
+  'HelpRequest':    "Las instrucciones disponibles actualmente son: Di, Define la variable, Muestra el tipo de, Muestra el valor de, Adiós Alfred. ¿Qué instrucción deseas que ejecute?",
+  'StopRequest':    "Cerrando la Skill... ¡Adiós!"
 };
 
 speak = (dialog) => DIALOGS.speak[0] + dialog + DIALOGS.speak[1];
 
-describe('[Unit Test] Lenguaje: Alfred', function() {
+describe('[Unit Test] Lenguaje: Alfred (es-ES)', function() {
 
   let alexa = VirtualAlexa.Builder()
     .handler("../" + lambda + "/index.handler")
@@ -23,6 +25,48 @@ describe('[Unit Test] Lenguaje: Alfred', function() {
   it('Launch Request', async function() {
     const res = await alexa.launch();
     expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.LaunchRequest));
+  });
+
+  it('Help Request', async function() {
+    let res;
+    res = await alexa.utter("ayuda");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.HelpRequest));
+    res = await alexa.utter("comandos");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.HelpRequest));
+    res = await alexa.utter("necesito ayuda");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.HelpRequest));
+  });
+
+  it('Stop Request', async function() {
+    let res;
+    res = await alexa.utter("salir");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+    res = await alexa.utter("salir de la skill");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+  });
+  
+  it('"Command: Say" Request', async function() {
+    let res;
+    res = await alexa.utter("di");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(""));
+    res = await alexa.utter("di di");
+    expect(res.response.outputSpeech.ssml).to.equal(speak("di"));
+    res = await alexa.utter("di joder");
+    expect(res.response.outputSpeech.ssml).to.equal(speak("joder"));
+    res = await alexa.utter("di hola mundo");
+    expect(res.response.outputSpeech.ssml).to.equal(speak("hola mundo"));
+  });
+
+  it('"Command: ByeAlfred" Request', async function() {
+    let res;
+    // FIXME: "Adios" Utterance Error
+    // res = await alexa.utter("adios");
+    // expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+    // FIXME: "Adios Alfred" Utterance Error
+    // res = await alexa.utter("adiós alfred");
+    // expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+    res = await alexa.utter("adiós alfred");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
   });
 });
 
