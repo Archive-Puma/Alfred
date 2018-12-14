d21f8d0be28bc2b9e2e54a915b75e355551d8e7e
MiniPuma <kikefontanlorenzo@gmail.com>
2018-12-14 01:25:58 +0100

    :white_check_mark: es-MX UnitTest

--- lambda/us-east-1_ask-custom-alexa-default/test.js
+++ lambda/us-east-1_ask-custom-alexa-default/test.js
@@ -90,4 +90,76 @@ describe('[Unit Test] Lenguaje: Alfred (es-ES)', function() {
   });
 });
 
+
+describe('[Unit Test] Lenguaje: Alfred (es-MX)', function() {
+
+  let alexa = VirtualAlexa.Builder()
+    .handler("../" + lambda + "/index.handler")
+    .interactionModelFile("../../models/es-MX.json")
+    .create();
+
+  it('Launch Request', async function() {
+    const res = await alexa.launch();
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.LaunchRequest));
+  });
+
+  it('Help Request', async function() {
+    let res;
+    res = await alexa.utter("ayuda");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.HelpRequest));
+    res = await alexa.utter("comandos");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.HelpRequest));
+    res = await alexa.utter("necesito ayuda");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.HelpRequest));
+  });
+
+  it('Stop Request', async function() {
+    let res;
+    res = await alexa.utter("salir");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+    res = await alexa.utter("salir de la skill");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+  });
+
+  it('"Command: Say" Request', async function() {
+    let res;
+    res = await alexa.utter("di");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(""));
+    res = await alexa.utter("di di");
+    expect(res.response.outputSpeech.ssml).to.equal(speak("di"));
+    res = await alexa.utter("di joder");
+    expect(res.response.outputSpeech.ssml).to.equal(speak("joder"));
+    res = await alexa.utter("di hola mundo");
+    expect(res.response.outputSpeech.ssml).to.equal(speak("hola mundo"));
+  });
+
+  it('"Command: Show" Request', async function() {
+    let res;
+    res = await alexa.utter("muestra el valor de");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.Show.undefined[0] + DIALOGS.Show.undefined[1]));
+    res = await alexa.utter("muestra el valor de x");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.Show.undefined[0] + "x" + DIALOGS.Show.undefined[1]));
+  });
+
+  it('"Command: ShowType" Request', async function() {
+    let res;
+    res = await alexa.utter("muestra el tipo de");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.Show.undefined[0] + DIALOGS.Show.undefined[1]));
+    res = await alexa.utter("muestra el tipo de x");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.Show.undefined[0] + "x" + DIALOGS.Show.undefined[1]));
+  });
+
+  it('"Command: ByeAlfred" Request', async function() {
+    let res;
+    // FIXME: "Adios" Utterance Error
+    // res = await alexa.utter("adios");
+    // expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+    // FIXME: "Adios Alfred" Utterance Error
+    // res = await alexa.utter("adiós alfred");
+    // expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+    res = await alexa.utter("adiós alfred");
+    expect(res.response.outputSpeech.ssml).to.equal(speak(DIALOGS.StopRequest));
+  });
+});
+
 // https://github.com/blutag-dev/alexa-unit-test-simple-mock
