6865dc7d6a7ce817c045ae31da2150421bc75041
PumaDeMenta <kikefontanlorenzo@gmail.com>
2019-05-17 01:59:21 +0200

    :snake: Modules inserted in the Python Path

--- src/alfred.py
+++ src/alfred.py
@@ -1,13 +1,19 @@
+import os
 import sys
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'lib'))
+
 from parser import parser
 
-def main():
-    if len(sys.argv) == 2:
-        with open(sys.argv[1],'r') as f:
-            print(parser.parse(f.read()))
 
-if __name__ == '__main__':
+def main():
     try:
-        main()
+        if len(sys.argv) == 2:
+            with open(sys.argv[1],'r') as f:
+                nodes = parser.parse(f.read())
+            for node in nodes:
+                node.eval()
     except KeyboardInterrupt:
         print()
+
+if __name__ == '__main__':
+    main()
