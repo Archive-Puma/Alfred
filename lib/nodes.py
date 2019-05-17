92d2b3a2f42283f5d15cae63e9b287230182e9e5
PumaDeMenta <kikefontanlorenzo@gmail.com>
2019-05-17 02:14:28 +0200

    :label: Some basic nodes

---
+++ lib/nodes.py
@@ -0,0 +1,34 @@
+class Node():
+    def eval(self):
+        raise NotImplementedError()
+
+class InstructionList(Node):
+    def __init__(self, child=list()):
+        self.child = child
+    def __repr__(self):
+        return "<InstructionList {}>".format(self.child)
+    def __iter__(self):
+        return iter(self.child)
+    def eval():
+        ret = list()
+        for instruction in self.child:
+            result = instruction.eval()
+            if res is not None:
+                ret.append(result)
+        return ret
+
+class Primitive(Node):
+    def __init__(self,value):
+        self.value = value
+    def __repr__(self):
+        return "<Primitive {} ({})>".format(self.value, self.value.__class__)
+    def eval(self):
+        return self.value
+
+class Stdout(Node):
+    def __init__(self,item):
+        self.item = item
+    def __repr__(self):
+        return "<Print {}>".format(self.item)
+    def eval(self):
+        print(self.item.eval())
